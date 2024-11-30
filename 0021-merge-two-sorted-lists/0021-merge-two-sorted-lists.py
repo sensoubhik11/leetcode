6e19a1d4cb0913ad97b07c4116eafcb179368ad8
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2024-12-01 00:26:20 +0530

    Time: 0 ms (100%), Space: 17.1 MB (15.22%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,27 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        curr_node = dummy
-        i, j = list1, list2
-        while i is not None and j is not None:
-            if i.val <= j.val:
-                curr_node.next = i
-                i = i.next
-            else:
-                curr_node.next = j
-                j = j.next
-            curr_node = curr_node.next
+        def merge_util(list1, list2):
+            if list1.next is None:
+                list1.next = list2
+                return list1
+            curr1, next1 = list1, list1.next
+            curr2, next2 = list2, list2.next
+
+            while next1 and curr2:
+                if curr2.val >= curr1.val and curr2.val <= next1.val:
+                    next2 = curr2.next
+                    curr1.next = curr2
+                    curr2.next = next1
+                    
+                    curr1 = curr2
+                    curr2 = next2
+                else:
+                    curr1 = next1
+                    next1 = curr1.next
+            if curr2 is not None:
+                curr1.next = curr2
+            return list1
         
-        while i is not None:
-            curr_node.next = i
-            i = i.next
-            curr_node = curr_node.next
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
         
-        while j is not None:
-            curr_node.next = j
-            j = j.next
-            curr_node = curr_node.next
-            
-        # the first node will have value 0, hence returning dummy.next
-        return dummy.next
+        if list1.val < list2.val:
+            return merge_util(list1, list2)
+        else:
+            return merge_util(list2, list1)
+
+                    
