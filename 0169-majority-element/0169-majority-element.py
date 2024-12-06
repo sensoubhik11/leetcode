b8a6395652ad1f1a3e50c0feaa173dd4c0865e55
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2024-12-06 23:25:56 +0530

    Time: 7 ms (57.82%), Space: 18.9 MB (8.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        votes = 0
         candidate = -1
+        votes = 0
         for num in nums:
             if votes == 0:
                 candidate = num
-                votes = 1
+                votes += 1
             else:
                 if num == candidate:
                     votes += 1
