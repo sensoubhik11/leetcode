00aba02ec6d93fb407265e543f693fd0308d578b
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2025-01-02 00:29:00 +0530

    Time: 4 ms (41.67%), Space: 18.6 MB (6.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,34 +3,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        m = len(matrix)
-        n = len(matrix[0])
-
-        is_first_col_zero = False
-        for i in range(m):
-            if matrix[i][0] == 0:
-                is_first_col_zero = True
-                break
-        is_first_row_zero = False
-        for j in range(n):
-            if matrix[0][j] == 0:
-                is_first_row_zero = True
-                break
-        
-        for i in range(1, m):
-            for j in range(1, n):
+        temp_mat = [row[:] for row in matrix]
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for i in range(rows):
+            for j in range(cols):
                 if matrix[i][j] == 0:
-                    matrix[i][0] = 0 # marking the row to be made zero
-                    matrix[0][j] = 0 # marking the col to be made zero
-        
-        for i in range(1, m):
-            for j in range(1, n):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-        
-        if is_first_col_zero:
-            for i in range(m):
-                matrix[i][0] = 0
-        if is_first_row_zero:
-            for j in range(n):
-                matrix[0][j] = 0
+                    for k in range(cols):
+                        temp_mat[i][k] = 0
+                    for l in range(rows):
+                        temp_mat[l][j] = 0
+        for i in range(rows):
+            for j in range(cols):
+                matrix[i][j] = temp_mat[i][j]
