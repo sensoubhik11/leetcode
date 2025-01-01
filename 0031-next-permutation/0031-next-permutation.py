1a5d9ca150b4ee6803becedeb5d06d36532c6d3f
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2025-01-01 23:33:47 +0530

    Time: 0 ms (100%), Space: 18 MB (6.45%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,17 +1,23 @@
 class Solution:
+    def reverse(self, nums, k):
+        low, high = k, len(nums)-1
+        while low < high:
+            nums[low], nums[high] = nums[high], nums[low]
+            low += 1
+            high -= 1
+            
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        i = n-1
-        while i-1 >=0 and nums[i-1] >= nums[i]:
-            i -= 1
-        if i == 0:
-            nums[:] = nums[::-1] #basically sort but since all are descending, using reverse
-        else:
-            j = len(nums)-1
-            while j >= i and nums[j] <= nums[i-1]:
-                j -= 1
-            nums[i-1], nums[j] = nums[j], nums[i-1]
-            nums[i:] = nums[i:][::-1]
+        k=0
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i] > nums[i-1]:
+                k = i
+                for j in range(len(nums)-1, i-1, -1):
+                    if nums[j] > nums[i-1]:
+                        nums[j], nums[i-1] = nums[i-1], nums[j]
+                        break
+                break
+        self.reverse(nums, k)
+        
