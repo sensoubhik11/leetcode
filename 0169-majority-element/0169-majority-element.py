73f203e38e6350fb8b58f4192f84a35b42b6a961
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2025-01-07 05:24:47 +0530

    Time: 11 ms (32.13%), Space: 19.2 MB (33.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        candidate = -1
-        votes = 0
+        n = len(nums)
+        freq = {}
         for num in nums:
-            if votes == 0:
-                candidate = num
-                votes += 1
+            if num in freq:
+                freq[num] += 1
             else:
-                if num == candidate:
-                    votes += 1
-                else:
-                    votes -= 1
-        return candidate
+                freq[num] = 1
+        for num in nums:
+            if freq[num] > n//2:
+                return num
