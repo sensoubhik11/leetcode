c6a3f0c728c065f596b1917b143c4243b8398f9c
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2025-01-06 12:39:33 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,15 @@
 class Solution:
-    def pow_util(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        a = self.pow_util(x, int(n/2))
-        if n%2 == 0:
-            return a*a
-        else:
-            return a*a*x
-
     def myPow(self, x: float, n: int) -> float:
-        ans = self.pow_util(x, n)
+        # deals with non-negative powers
+        def cal_pow(x, n):
+            if n == 0:
+                return 1
+            a = cal_pow(x, n//2)
+            if n%2 == 0:
+                return a*a
+            else:
+                return x*a*a
+        ans = cal_pow(x,abs(n))
         if n < 0:
-            ans = 1/ans
+            return 1/ans
         return ans
