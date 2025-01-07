936c3cf80b1d3f42ee093e37531f4a03365abfe0
Soubhik Sen <ss.24p10223@nitdgp.ac.in>
2025-01-07 05:37:57 +0530

    Time: 15 ms (17.92%), Space: 19.1 MB (33.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n = len(nums)
-        freq = {}
+        freq = defaultdict(int)
         for num in nums:
-            if num in freq:
-                freq[num] += 1
-            else:
-                freq[num] = 1
-        for num in nums:
+            freq[num] += 1
             if freq[num] > n//2:
                 return num
+        
+            
